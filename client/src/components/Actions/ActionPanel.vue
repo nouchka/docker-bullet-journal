048a420ec614f4e9b65af986f4b8cb2ed68f2d02
vkwho9ar <vkwho9ar@gmail.com>
2021-07-30 16:38:12 -0400

    edited background colour of panel

--- client/src/components/Actions/ActionPanel.vue
+++ client/src/components/Actions/ActionPanel.vue
@@ -1,186 +1,180 @@
 <template>
-   <div id="action-panel" class="panel" :class="{'show':showWhichPanel === 'action'}">
-       <div class="image-container">
-            <div class="image-box">
-                <MorningIcon class="morning-icon" />
-            </div>
-            <div class='date-box'>
-                <h2>{{today}}</h2>
-            </div>
-       </div>
-       <CreateItem />
-       <CreateHabit />
-    </div>        
+  <div
+    id="action-panel"
+    class="panel"
+    :class="{ show: showWhichPanel === 'action' }"
+  >
+    <div class="image-container">
+      <div class="image-box">
+        <MorningIcon class="morning-icon" />
+      </div>
+      <div class="date-box">
+        <h2>{{ today }}</h2>
+      </div>
+    </div>
+    <CreateItem />
+    <CreateHabit />
+  </div>
 </template>
 
 <script>
-import CreateItem from "./CreateItem"
-import CreateHabit from "./CreateHabit"
-import MorningIcon from "./MorningIcon"
-import {mapGetters, mapActions} from "vuex"
+import CreateItem from "./CreateItem";
+import CreateHabit from "./CreateHabit";
+import MorningIcon from "./MorningIcon";
+import { mapGetters, mapActions } from "vuex";
 
 export default {
-    name: "ActionPanel",
-    components: {
-        CreateItem,
-        CreateHabit,
-        MorningIcon,
-    },
-    data () {
-        return {
-            today: new Intl.DateTimeFormat('en-us', {weekday: "long", year: "numeric", month: "long", day: "numeric"}).format( new Date)
-        }
-    },
-    computed: {
-        ...mapGetters(["showWhichPanel", "getMobileMode"]),   
-    },
-    methods: mapActions(["changePanel"]),
-}
+  name: "ActionPanel",
+  components: {
+    CreateItem,
+    CreateHabit,
+    MorningIcon,
+  },
+  data() {
+    return {
+      today: new Intl.DateTimeFormat("en-us", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      }).format(new Date()),
+    };
+  },
+  computed: {
+    ...mapGetters(["showWhichPanel", "getMobileMode"]),
+  },
+  methods: mapActions(["changePanel"]),
+};
 </script>
 
 <style scoped>
-
 #action-panel {
-    display: grid;
-    grid-template-rows: 3fr 6fr 4fr;
-    grid-gap: 10rem;
-    overflow: hidden;
+  display: grid;
+  grid-template-rows: 3fr 6fr 4fr;
+  grid-gap: 10rem;
+  overflow: hidden;
 }
 
 h2 {
-    font-size: 20rem;
-    font-weight: 500;
+  font-size: 20rem;
+  font-weight: 500;
 }
 
 .image-container {
-    position: relative;
-    border-radius: 10rem;
-    overflow: hidden;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
+  position: relative;
+  border-radius: 10rem;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
 }
 
 .morning-icon {
-    height: 100%;
+  height: 100%;
 }
 
 .image-box {
-    position: relative;
-    height: 100%;
-    border-radius: 10%;
-    min-width: fit-content;
-    overflow: hidden;
+  position: relative;
+  height: 100%;
+  border-radius: 10%;
+  min-width: fit-content;
+  overflow: hidden;
 }
 
 .date-box {
-    margin-top: 10rem;
-    padding-left: 10rem;
+  margin-top: 10rem;
+  padding-left: 10rem;
 }
 
-
-
-
 @media only screen and (max-width: 1400px), (max-height: 600px) {
-    #action-panel {
-        position: absolute;
-        z-index: 10;
-        width: 100%;
-        height: 40%;
-        bottom: -100%;
-        padding: 15rem;
-        grid-gap: 20rem;
-        grid-template-rows: none;
-        grid-template-columns: 260rem 1fr 1fr;
-        backdrop-filter: blur(10rem);
-        transition: all 1s;
-        box-shadow: 0 -3rem 10rem 5rem rgb(25, 81, 94, 0.2);
-    }
+  #action-panel {
+    position: absolute;
+    z-index: 10;
+    width: 100%;
+    height: 40%;
+    bottom: -100%;
+    padding: 15rem;
+    grid-gap: 20rem;
+    grid-template-rows: none;
+    grid-template-columns: 260rem 1fr 1fr;
+    transition: all 1s;
+    box-shadow: 0 -3rem 10rem 5rem rgb(25, 81, 94, 0.2);
+    background: rgb(195, 224, 221);
+  }
 
-    .image-container {
-        flex-direction: column;
-        align-items: flex-start;
-    }
+  .image-container {
+    flex-direction: column;
+    align-items: flex-start;
+  }
 
-    .image-box {
-        height: 70%;
-        max-height: 260rem;
-    }
+  .image-box {
+    height: 70%;
+    max-height: 260rem;
+  }
 
-    h2 {
-        font-size: 18rem;
-    }
+  h2 {
+    font-size: 18rem;
+  }
 
-    .morning-icon {
-        height: 100%;
-    }
+  .morning-icon {
+    height: 100%;
+  }
 
-    .date-box {
-        width: 100%;
-    }
+  .date-box {
+    width: 100%;
+  }
 
-    #action-panel.show {
-        bottom: 0;
-    }
-
+  #action-panel.show {
+    bottom: 0;
+  }
 }
 
 @media only screen and (max-width: 1000px), (max-height: 600px) {
+  #action-panel {
+    width: 95%;
+    height: 100%;
+    padding: 10rem;
+    grid-template-rows: 3fr 6fr 4fr;
+    grid-template-columns: none;
+  }
 
-    #action-panel {
-         width: 95%;
-         height: 100%;
-         padding: 10rem;
-         grid-template-rows: 3fr 6fr 4fr;
-         grid-template-columns: none;
-    }
+  .image-container {
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+  }
 
-    .image-container {
-        flex-direction: row;
-        align-items: center;
-        justify-content: flex-start;
-    }
+  .image-box {
+    height: 100%;
+  }
 
-    .image-box {
-        height: 100%;
-    }
+  h2 {
+    font-size: calc(min(20rem, 4.4vw));
+  }
 
-    h2 {
-        font-size: calc(min(20rem, 4.4vw));
-    }
-
-    .date-box {
-        padding-left: 10rem;
-    } 
-
+  .date-box {
+    padding-left: 10rem;
+  }
 }
-
 </style>
 
 <style>
-
-
 #action-panel .header {
-    height: 30rem;
-    padding: 5rem 10rem;
-    border-radius: 10rem 10rem 0 0;
+  height: 30rem;
+  padding: 5rem 10rem;
+  border-radius: 10rem 10rem 0 0;
 }
 
 input {
   padding: 7rem 0;
 }
 
-
 #action-panel button {
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    width: 96%;
-    margin-left: 2%;
-    
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 96%;
+  margin-left: 2%;
 }
-
-
-
 </style>
